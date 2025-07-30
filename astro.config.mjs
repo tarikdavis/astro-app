76bc4d45612864c777f69201f30c9a8be952466a
tarikdavis <tarikdavis@hotmail.co.uk>
2025-07-30 08:48:58 +0100

    fixed padding around components, updated css to include missing tailwind classes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
---
+++ src/components/companies.tsx
@@ -0,0 +1,84 @@
+import { Timer, Zap, ZoomIn } from "lucide-react";
+
+const Feature16 = () => {
+  return (
+    <section className="py-16">
+      <div className="container">
+
+        <h2 className="text-3xl font-medium lg:text-4xl">Companies I've worked with</h2>
+        <div className="mt-14 grid gap-6 lg:mt-20 lg:grid-cols-3">
+          <div className="rounded-lg bg-accent p-5">
+            <span className="mb-8 flex size-12 items-center justify-center rounded-full bg-background">
+              <Timer className="size-6" />
+            </span>
+            <h3 className="mb-2 text-xl font-medium">HSBC</h3>
+            <p className="leading-7 text-muted-foreground">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+              beatae tenetur totam aut blanditis ipsa quaerat neque eaque, atque
+              doloremque! Eligendi.
+            </p>
+          </div>
+          <div className="rounded-lg bg-accent p-5">
+            <span className="mb-8 flex size-12 items-center justify-center rounded-full bg-background">
+              <ZoomIn className="size-6" />
+            </span>
+            <h3 className="mb-2 text-xl font-medium">Lloyds Bank</h3>
+            <p className="leading-7 text-muted-foreground">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+              beatae tenetur totam aut blanditis ipsa quaerat neque eaque, atque
+              doloremque! Eligendi.
+            </p>
+          </div>
+          <div className="rounded-lg bg-accent p-5">
+            <span className="mb-8 flex size-12 items-center justify-center rounded-full bg-background">
+              <Zap className="size-6" />
+            </span>
+            <h3 className="mb-2 text-xl font-medium">Virgin Media</h3>
+            <p className="leading-7 text-muted-foreground">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+              beatae tenetur totam aut blanditis ipsa quaerat neque eaque, atque
+              doloremque! Eligendi.
+            </p>
+          </div>
+        </div>
+        <div className="mt-14 grid gap-6 lg:mt-20 lg:grid-cols-3">
+          <div className="rounded-lg bg-accent p-5">
+            <span className="mb-8 flex size-12 items-center justify-center rounded-full bg-background">
+              <Timer className="size-6" />
+            </span>
+            <h3 className="mb-2 text-xl font-medium">Aviva</h3>
+            <p className="leading-7 text-muted-foreground">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+              beatae tenetur totam aut blanditis ipsa quaerat neque eaque, atque
+              doloremque! Eligendi.
+            </p>
+          </div>
+          <div className="rounded-lg bg-accent p-5">
+            <span className="mb-8 flex size-12 items-center justify-center rounded-full bg-background">
+              <ZoomIn className="size-6" />
+            </span>
+            <h3 className="mb-2 text-xl font-medium">Barclays Bank</h3>
+            <p className="leading-7 text-muted-foreground">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+              beatae tenetur totam aut blanditis ipsa quaerat neque eaque, atque
+              doloremque! Eligendi.
+            </p>
+          </div>
+          <div className="rounded-lg bg-accent p-5">
+            <span className="mb-8 flex size-12 items-center justify-center rounded-full bg-background">
+              <Zap className="size-6" />
+            </span>
+            <h3 className="mb-2 text-xl font-medium">BBC</h3>
+            <p className="leading-7 text-muted-foreground">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+              beatae tenetur totam aut blanditis ipsa quaerat neque eaque, atque
+              doloremque! Eligendi.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export { Feature16 };
