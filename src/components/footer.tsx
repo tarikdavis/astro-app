8f90b7e42ace3f2587ceb4367b5389f09dda8d60
tarikdavis <tarikdavis@hotmail.co.uk>
2025-07-30 09:02:03 +0100

    Added Footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,59 @@
+import { ArrowUpRight } from "lucide-react";
+
+const Footer12 = () => {
+  const navigation = [
+
+    { name: "About", href: "#" },
+    { name: "Latest work", href: "#" },
+    { name: "Contact", href: "#" },
+  ];
+
+  const social = [
+    { name: "LinkedIn", href: "#" },
+  ];
+
+  const legal = [{ name: "Privacy Policy", href: "#" }];
+
+  return (
+    <section className="flex flex-col items-center gap-14 py-32">
+      <nav className="container flex flex-col items-center gap-4">
+        <ul className="flex flex-wrap items-center justify-center gap-6">
+          {navigation.map((item) => (
+            <li key={item.name}>
+              <a
+                href={item.href}
+                className="font-medium transition-opacity hover:opacity-75"
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+          {social.map((item) => (
+            <li key={item.name}>
+              <a
+                href={item.href}
+                className="flex items-center gap-0.5 font-medium transition-opacity hover:opacity-75"
+              >
+                {item.name} <ArrowUpRight className="size-4" />
+              </a>
+            </li>
+          ))}
+        </ul>
+        <ul className="flex flex-wrap items-center justify-center gap-6">
+          {legal.map((item) => (
+            <li key={item.name}>
+              <a
+                href={item.href}
+                className="text-sm text-muted-foreground transition-opacity hover:opacity-75"
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </section>
+  );
+};
+
+export { Footer12 };
