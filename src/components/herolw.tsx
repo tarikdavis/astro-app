1ee5f68f970c2b1bb8a9563dcb2049a9c2ec2c01
tarikdavis <tarikdavis@hotmail.co.uk>
2025-07-31 13:06:23 +0100

    changed hero on latest work to match index

--- src/components/herolw.tsx
+++ src/components/herolw.tsx
@@ -1,83 +1,38 @@
-import { ArrowRight, ArrowUpRight } from "lucide-react";
+import {
+  ArrowRight,
+  Blocks,
+  MessagesSquare,
+  MoveRight,
+  Users,
+} from "lucide-react";
 
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 
-interface Hero1Props {
-  badge?: string;
-  heading: string;
-  description: string;
-  buttons?: {
-    primary?: {
-      text: string;
-      url: string;
-    };
-    secondary?: {
-      text: string;
-      url: string;
-    };
-  };
-  image: {
-    src: string;
-    alt: string;
-  };
-}
-
-const Hero1 = ({
-  badge = "Latest work",
-  heading = "HSBC Mortgage Finder",
-  description = "A comprehensive mortgage finder tool designed to help customers discover the right mortgage options through an intuitive, user-friendly interface with advanced calculation capabilities.",
-  buttons = {
-    primary: {
-      text: "View live project",
-      url: "https://www.hsbc.co.uk/mortgages/finder/",
-    },
-    secondary: {
-      text: "Discuss this project",
-      url: "/contact",
-    },
-  },
-  image = {
-    src: "https://github.com/tarikdavis/astro-app/blob/main/src/img/mortgage-product-finder.webp?raw=true",
-    alt: "HSBC Mortgage Finder hero image",
-  },
-}: Hero1Props) => {
+const Hero26 = () => {
   return (
-    <section className="py-12 bg-primary-background">
+    <section className="py-12">
       <div className="container">
-        <div className="grid items-center gap-8 lg:grid-cols-2">
-          <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
-            <h1 className="my-6 text-pretty text-4xl font-semibold lg:text-6xl">
-              {heading}
-            </h1>
-            <p className="text-muted-foreground mb-8 max-w-xl lg:text-xl">
-              {description}
-            </p>
-            <div className="flex w-full flex-col justify-center gap-2 sm:flex-row lg:justify-start">
-              {buttons.primary && (
-                <Button asChild className="w-full sm:w-auto">
-                  <a href={buttons.primary.url}>{buttons.primary.text}</a>
-                </Button>
-              )}
-              {buttons.secondary && (
-                <Button asChild variant="outline" className="w-full sm:w-auto">
-                  <a href={buttons.secondary.url}>
-                    {buttons.secondary.text}
-                    <ArrowRight className="size-4" />
-                  </a>
-                </Button>
-              )}
-            </div>
+        <div className="text-center">
+
+          <h1 className="mx-auto mt-4 mb-3 max-w-3xl text-4xl font-semibold text-balance lg:mb-7 lg:text-7xl">
+            HSBC Mortgage Finder
+          </h1>
+          <p className="m mx-auto max-w-3xl text-muted-foreground lg:text-xl">
+          A comprehensive mortgage finder tool designed to help customers discover the right mortgage options through an intuitive, user-friendly interface with advanced calculation capabilities.
+          </p>
+          <div className="mt-8 flex flex-col justify-center gap-4 sm:flex-row">
+            <Button size="lg">
+              View live project<MoveRight className="ml-2" strokeWidth={1} />
+            </Button>
+            <Button size="lg" variant="ghost">
+              Discuss this project
+              <MoveRight className="ml-2" strokeWidth={1} />
+            </Button>
           </div>
-          <img
-            src={image.src}
-            alt={image.alt}
-            className="max-h-96 w-full rounded-md object-cover"
-          />
         </div>
       </div>
     </section>
   );
 };
 
-export { Hero1 };
+export { Hero26 };
