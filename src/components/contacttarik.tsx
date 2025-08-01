82df5ea15afe99577c0ab7716744bfa9888b207c
tarikdavis <tarikdavis@hotmail.co.uk>
2025-08-01 08:44:53 +0100

    tidy up files

--- src/components/contacttarik.tsx
+++ src/components/contacttarik.tsx
@@ -12,7 +12,7 @@ const Contact5 = () => {
             Get in touch
           </h1>
         </div>
-        <form name="contact" method="POST" data-netlify="true">
+        <form method="POST">
         <div className="mx-auto flex max-w-lg flex-col gap-6">
           
           <div className="grid w-full items-center gap-2">
